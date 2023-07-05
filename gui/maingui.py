1e1bb0986c3c64415d9d34b7c3a896a6b9d6f45f
Daniel Lietz <ge96jek@mytum.de>
2023-07-05 19:17:59 +0200

    implemented undo and redo, folder clicking, file opening (needs testing on windows)

--- gui/maingui.py
+++ gui/maingui.py
@@ -3,13 +3,52 @@ import os
 from tkinter import ttk
 from .folderinfo import returnItems
 from .datatypes import Bytes
-
+import webbrowser
 
 class fileGui:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Filesystem GUI")
         self.tree = ttk.Treeview(self.window)
+        self.tree.bind("<Double-Button-1>", self.item_double_clicked)
+        self.undoStack = []
+        self.redoStack = []
+        self.curDir = os.getcwd() #TODO: Update to root?
+        self.back_button = tk.Button( self.window, text="Back", command=self.go_back)
+        self.back_button.pack(side=tk.LEFT)
+        self.back_button.configure(state='disabled')
+        
+        self.forward_button = tk.Button( self.window, text="Forward", command=self.go_forward)
+        self.forward_button.pack(side=tk.LEFT)
+        self.forward_button.configure(state='disabled')
+        
+    def buildTree(self):
+        for file in returnItems(self.curDir):
+            fileName, fileSize, lastModified = file[0], file[1], file[2]
+            id = self.tree.insert("", tk.END, values=(fileName, fileSize, lastModified))
+
+
+
+    def item_double_clicked(self,event):
+        item = self.tree.focus()
+        if item:
+            item_text = self.tree.item(item,"values")
+            newDir = os.path.join(self.curDir,item_text[0])
+            if os.path.isfile(newDir):
+                #TODO Does this work on Windows?
+                webbrowser.open(newDir)
+            else:
+                
+                self.undoStack.append((self.curDir,[self.tree.item(x) for x in self.tree.get_children()]))
+                self.curDir = os.path.join(self.curDir,item_text[0])
+                self.back_button.configure(state='active')
+                self.forward_button.configure(state='disabled')
+                self.redoStack = []
+                self.reset_tree()
+                self.buildTree()
+
+                
+            
 
     def sort_by_column(self, column, descending):
         data = [
@@ -28,12 +67,37 @@ class fileGui:
         for index, (_, child) in enumerate(data):
             self.tree.move(child, "", index)
 
+    def go_back(self):
+        print(self.curDir)
+        oldState = self.undoStack.pop()
+        if len(self.undoStack) == 0:
+            self.back_button.configure(state='disabled')
+        self.redoStack.append((self.curDir,[self.tree.item(x) for x in self.tree.get_children()])) 
+       
+        self.reset_tree()
+        for item in oldState[1]:
+            self.tree.insert('', tk.END, **item)
+        self.curDir = oldState[0]
+        self.forward_button.config(state='active')  
+        print(self.curDir)
+        
+
+    def go_forward(self):
+        oldState = self.redoStack.pop()
+        if len(self.redoStack) == 0:
+            self.forward_button.config(state='disabled')
+        self.undoStack.append((self.curDir,[self.tree.item(x) for x in self.tree.get_children()]))
+        self.reset_tree()
+        for item in oldState[1]:
+            self.tree.insert('', tk.END, **item)
+        self.curDir = oldState[0]
+        self.back_button.config(state='active')
+        
+        print(self.curDir)
+
     def reset_tree(self):
         self.tree.delete(*self.tree.get_children())  # Delete all items
-        columns = self.tree["columns"]
-        for column in columns:
-            self.tree.column(column, width=0)
-
+        
     def on_column_click(self, column):
         descending = False
         if column == self.tree.previous_sort_column:
@@ -48,7 +112,6 @@ class fileGui:
         self.tree["columns"] = ("file_name", "file_size", "last_modified")
         self.tree.heading("#0", text="", anchor="w")
         self.tree.column("#0", width=0, stretch=tk.NO)
-
         self.tree.heading(
             "file_name",
             text="File Name",
@@ -74,6 +137,7 @@ class fileGui:
 
         for file in returnItems(os.getcwd()):
             fileName, fileSize, lastModified = file[0], file[1], file[2]
+            id = self.tree.insert("", tk.END, values=(fileName, fileSize, lastModified))
 
         self.tree.pack(fill=tk.BOTH, expand=True)
 
